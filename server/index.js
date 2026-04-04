e407780037295e865336f2cdd6419ad5a07a1409
devadath006 <devadathme@gmail.com>
2026-04-04 10:53:10 +0530

    reset

--- server/index.js
+++
@@ -1,5 +0,0 @@
-app.get('/api/members', async (req, res) => {
-    const { data, error } = await supabase.from('iei_members').select('*');
-    if (error) return res.status(500).json(error);
-    res.json(data);
-});
